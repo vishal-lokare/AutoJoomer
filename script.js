1ec9595e61e708deab545bf2d104ebf69b7695b4
vishal-lokare <31512838+vishal-lokare@users.noreply.github.com>
2021-07-22 00:33:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,13 +43,15 @@ $.getJSON('http://www.vishal-lokare.co/AutoJoomer/index.html', function(links) {
             millisOfThatClass[i] = new Date(now.getFullYear(), now.getMonth(), now.getDate(), h[i], m[i], 0, 0) - now;
             if(millisOfThatClass[i] > 0) {
                     setTimeout(function() {
-                        alert( 'Now the class is ' + thatClassName[i] + ' at ' + millisOfThatClass + ' ' + i);
-                        window.open(thatClassLink[i],"_blank");
+                        if(window.confirm( 'Now the class is ' + thatClassName[i] + ' at ' + millisOfThatClass + ' ' + i)){
+                                window.open(thatClassLink[i],"_blank");
+                        }
                     }, millisOfThatClass[i]);
             }
             else if(millisOfThatClass[i] > -2400000) {
-                alert( 'Now the class is ' + thatClassName[i] + ' at ' + millisOfThatClass + ' ' + i);
-                window.open(thatClassLink[i],"_blank");
+                if(window.confirm( 'Now the class is ' + thatClassName[i] + ' at ' + millisOfThatClass + ' ' + i)){
+                        window.open(thatClassLink[i],"_blank");
+                }
             }
         }
 });
