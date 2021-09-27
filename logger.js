af6495bfd0123caa2705854638941002386511d8
ADITYA SRINIVAS MENON <adityasrinivas.menon@gmail.com>
2021-09-27 12:29:44 +0530

    new code, better time management'

--- logger.js
+++ logger.js
@@ -21,7 +21,8 @@ $(document).ready(function () {
         document.getElementsByClassName('btn btn-primary btn-block mt-3')[0].click();
     }
 
-    if (url.includes('https://lms.iiitkottayam.ac.in/mod/bigbluebuttonbn/view.php?id='))
+
+    if (url.includes('?reason=AutoJoomer')){
         var btn = document.getElementById("join_button_input");
         
         var timerId = setInterval(function(){ 
@@ -35,8 +36,5 @@ $(document).ready(function () {
                      console.log("new var selected")
                }
         }, 1000);
-
-
-    if (url.includes('?reason=AutoJoomer'))
-        setTimeout(function () { document.getElementById("join_button_input").click(); }, 5000);
+    }
 })
