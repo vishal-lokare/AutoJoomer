0a8d57fdd434da282224960b4b2d41d3fb868768
nogi2k2 <ashutoshrai20bcs20@iiitkottayam.ac.in>
2021-10-14 18:02:44 +0530

    Full page commit no 2

--- timetable.js
+++ timetable.js
@@ -3,15 +3,17 @@ window.onload=function(){
 		$.getJSON(fetchLink, function (links){
 			for(j=1;j<=5;j++){
 				var thatDay = (links[j])
-				var nooflec = Object.keys(thatDay).length-1;  
-				for (let i = 1; i <= nooflec-1; i++) {
+				for (let i=1;i<=5;i++) {
 					if(i==5){
 						var thatClass = thatDay[i];
 						var thatClassName = thatClass['class_name'];
 						var thatClassLink = thatClass['class_link'];  
-						id='d'+j+'l'
+						id='d'+j+'l';
 						str='<a href='+thatClassLink+'>'+thatClassName+'</a>'
-                		document.getElementById(id).innerHTML=str;
+						var exec=document.getElementById(id);
+						if(exec!=null){
+							exec.innerHTML=str;
+						}	
 					}
 					else{
 						var thatClass = thatDay[i];
