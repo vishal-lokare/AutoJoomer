677945354213a6599355929cfc2ea4d0ee313dde
nogi2k2 <ashutoshrai20bcs20@iiitkottayam.ac.in>
2021-11-08 18:18:17 +0530

    Added reset/reload button for popup and automatically closing already opened lms class tabs check

--- logger.js
+++ logger.js
@@ -21,11 +21,25 @@ $(document).ready(function () {
         document.getElementsByClassName('btn btn-primary btn-block mt-3')[0].click();
     }
 
-
     if (url.includes('?reason=AutoJoomer')){
-        var btn = document.getElementById("join_button_input");
+        var ctr=0;
+            function getAllInWindow(){
+                chrome.tabs.query({}, function(tabs) {
+                    for (let i = 0, tab; tab = tabs[i]; i++) {
+                        if (tab.url.includes('https://ocs.iiitkottayam.ac.in/html5client/')){
+                            window.open('','_parent',''); 
+                            window.close();
+                            // chrome.tabs.update(tab.id, {active: true});
+                            ctr+=1;
+                            return;
+                        }
+                    }
+            });
+        }
+        if (!ctr){
+            var btn = document.getElementById("join_button_input");
         
-        var timerId = setInterval(function(){ 
+            var timerId = setInterval(function(){ 
                if(btn){
                      btn.click();
                      clearInterval(timerId);
@@ -34,5 +48,6 @@ $(document).ready(function () {
                      btn = document.getElementById("join_button_input");
                }
         }, 1000);
+        }
     }
 })
