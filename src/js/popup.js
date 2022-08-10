599034b5eea90dfd39fae6338d34c19266dd891c
Om Ghumare <omprashant20bcs130@iiitkottayam.ac.in>
2022-08-10 14:13:26 +0530

    New design

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,123 +1,199 @@
-//global variables
-var username;
-var password;
-var branch;
-var confirmation;
-var earlyjoin;
+let checker = 0;
+
 window.onload = function () {
-  populator();
-  // Click event listener for Login to Wi-Fi button
-  document.getElementById("wifiLogin").addEventListener("click", function () {
-    window.open("https://msftconnecttest.com/redirect"), "_blank";
-  });
-  document.getElementById("button").addEventListener("click", saveValues);
-  document.getElementById("button").innerHTML = "SAVE";
-  document.getElementById("button").disabled = false;
-  document.getElementById("year").addEventListener("change", function () {
-    if (year.value == "2019") {
-      branch.getElementsByTagName("option")[1].selected = true;
-      batch.getElementsByTagName("option")[0].selected = true;
-      branch.disabled = true;
-      batch.disabled = true;
-    } else {
-      branch.getElementsByTagName("option")[1].selected = true;
-      batch.getElementsByTagName("option")[1].selected = true;
-      branch.disabled = false;
-      batch.disabled = false;
-    }
-  });
+    theme();//checks theme
+    // pingURL();
+    //isOnline();
+  
+}
 
-  ghManifestLink = "https://www.vishal-lokare.co/AutoJoomer/manifest.json";
-  $.getJSON(ghManifestLink, function (links) {
-    var thisVersion = chrome.runtime.getManifest().version;
-    if (links["version"] != thisVersion) {
-      var newUpdate = "none";
-      $.getJSON(
-        "https://www.vishal-lokare.co/AutoJoomer/version_updates.json",
-        function (ver) {
-          newUpdate = ver[links["version"]];
-          if (
-            window.confirm(
-              "New update : " +
-                links["version"] +
-                " available.\nCurrent version : " +
-                thisVersion +
-                "\nPlease update from GH repo.\n\nNew in this update :\n" +
-                newUpdate
-            )
-          ) {
-            window.open(
-              "https://github.com/vishal-lokare/AutoJoomer",
-              "_blank"
-            );
-          }
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("nav_button").addEventListener("click",mode_change)
+   
+});
+   
+
+// function getRandomString () {
+    //     return Math.random().toString(36).substring(2, 15)
+    //   }
+      
+    //   async function isOnline () {
+    //     if (!window.navigator.onLine){
+    //         conn_status=0;
+    //         return false
+    //     }
+    //     // avoid CORS errors with a request to your own origin
+    //     const url = new URL(window.location.origin)
+      
+    //     // random value to prevent cached responses
+    //     url.searchParams.set('rand', getRandomString())
+      
+    //     try {
+    //       const response = await fetch(
+    //         url.toString(),
+    //         { method: 'HEAD' },
+    //       )
+      
+    //       console.log(response.ok);
+    //       conn_status=1;
+    //       return response.ok
+    //     } catch {
+    //         conn_status=0;
+    //       return false
+    //     }
+    //   }
+
+// function pingURL() {
+
+//     // The custom URL entered by user
+//     var URL = "https://api.publicapis.org/entries";
+//     console.log("url = " + URL);
+//     var settings = {
+    
+//         // Defines the configurations
+//         // for the request
+//         cache: false,
+//         dataType: "html",
+//         async: true,
+//         crossDomain: true,
+//         url: URL,
+//         method: "GET",
+//         timeout: 10000,
+//         headers: {
+//         accept: "*",
+//         "Access-Control-Allow-Origin": "*",
+//         },
+    
+//         // Defines the response to be made
+//         // for certain status codes
+//         statusCode: {
+//         200: function (response) {
+//                 // let offline=document.getElementById("dis_conn")
+//                 // let online=document.getElementById("wifi_conn")
+//                 // online.style.display="flex";
+//                 // offline.style.display="none";
+//                 console.log("on");
+//         },
+//         400: function (response) {
+//             // let offline=document.getElementById("dis_conn")
+//             // let online=document.getElementById("wifi_conn")
+//             // online.style.display="none";
+//             // offline.style.display="flex";
+//             console.log("off");
+//         },
+//         0: function (response) {
+//                 // let offline=document.getElementById("dis_conn")
+//                 // let online=document.getElementById("wifi_conn")
+//                 // online.style.display="none";
+//                 // offline.style.display="flex";
+//                 console.log("off");
+//         },
+//         },
+//     };
+    
+//     // Sends the request and observes the response
+//     $.ajax(settings)
+//     .done(function (response) {
+//         console.log(response);
+//     })
+//     .fail(function (response) {
+//         console.log("Error " + response);
+//     });
+//  }
+
+
+function mode_change() {
+
+        let element1 = document.body;
+        element1.classList.toggle("dark_bg");
+
+        if(checker)
+        {
+            window.localStorage.setItem("dark_check", 0);
+            checker = 0;
         }
-      );
+        else
+        {
+            window.localStorage.setItem("dark_check", 1);
+            checker = 1;
+        }
+        
+
+        let element2 = document.getElementById("reminders")
+        console.log(element2)
+
+        if(checker === 1)
+        {
+            element2.classList.remove("sections");
+            element2.classList.add("dark_content")
+        }
+        else
+        {
+            element2.classList.remove("dark_content")
+            element2.classList.add("sections");
+        }
+
+
+        let element3 = document.getElementsByClassName("foot_box")
+        for (let i = 0; i < element3.length; i++) {
+            if (element3[i].classList.contains("foot_item")) {
+
+                element3[i].classList.remove("foot_item");
+                element3[i].classList.add("dark_foot");
+                console.log("going to dark")
+
+            }
+            else {
+                element3[i].classList.remove("dark_foot")
+                element3[i].classList.add("foot_item");
+                console.log("dark to light");
+                
+            }
+        }
+        
+    };
+
+function theme(){
+    console.log("theme")
+
+    const mode = window.localStorage.getItem("dark_check")
+    // console.log("mode", mode)
+    if(mode === null || mode == 0);
+    else
+    {
+        console.log("Changing mode ...", mode)
+        mode_change();
     }
-  });
-};
-
-function saveValues() {
-  username = document.getElementById("username");
-  password = document.getElementById("password");
-  branch = document.getElementById("branch");
-  earlyjoin = document.getElementById("earlyjoin");
-  confirmation = document.getElementById("confirmation");
-  document.getElementById("button").style.color = "white";
-  document.getElementById("button").innerHTML = "Restart Browser";
-  document.getElementById("button").disabled = true;
-  var uname = username.value;
-  var pass = password.value;
-  var uyear = document.getElementById("year").value;
-  var ubatch = document.getElementById("batch").value;
-  var ejoin = document.getElementById("earlyjoin").value;
-
-  var bran = branch.value;
-  if (document.getElementById("confirmation").checked) conf = 1;
-  else conf = 0;
-  window.localStorage.setItem("AutoJoomerUsername", JSON.stringify(uname));
-  window.localStorage.setItem("AutoJoomerPassword", JSON.stringify(pass));
-  window.localStorage.setItem("AutoJoomerYear", JSON.stringify(uyear));
-  window.localStorage.setItem("AutoJoomerBranch", JSON.stringify(bran));
-  window.localStorage.setItem("AutoJoomerBatch", JSON.stringify(ubatch));
-  window.localStorage.setItem("AutoJoomerEarlyJoin", JSON.stringify(ejoin));
-  window.localStorage.setItem("AutoJoomerConfirmation", JSON.stringify(conf));
 }
 
-function populator() {
-  username = document.getElementById("username");
-  password = document.getElementById("password");
-  year = document.getElementById("year");
-  branch = document.getElementById("branch");
-  batch = document.getElementById("batch");
-  earlyjoin = document.getElementById("earlyjoin");
-  confirmation = document.getElementById("confirmation");
-  //setting the values
-  if (JSON.parse(window.localStorage.getItem("AutoJoomerUsername")) != null)
-    username.value = JSON.parse(
-      window.localStorage.getItem("AutoJoomerUsername")
-    );
-  if (JSON.parse(window.localStorage.getItem("AutoJoomerPassword")) != null)
-    password.value = JSON.parse(
-      window.localStorage.getItem("AutoJoomerPassword")
-    );
-  if (JSON.parse(window.localStorage.getItem("AutoJoomerYear")) != null)
-    year.value = JSON.parse(window.localStorage.getItem("AutoJoomerYear"));
-  if (year.value == "2019") {
-    branch.disabled = true;
-    batch.disabled = true;
-  }
-  if (JSON.parse(window.localStorage.getItem("AutoJoomerBranch")) != null)
-    branch.value = JSON.parse(window.localStorage.getItem("AutoJoomerBranch"));
-  if (JSON.parse(window.localStorage.getItem("AutoJoomerBatch")) != null)
-    batch.value = JSON.parse(window.localStorage.getItem("AutoJoomerBatch"));
-  if (JSON.parse(window.localStorage.getItem("AutoJoomerEarlyJoin")) != null)
-    earlyjoin.value = JSON.parse(
-      window.localStorage.getItem("AutoJoomerEarlyJoin")
-    );
 
-  if (JSON.parse(window.localStorage.getItem("AutoJoomerConfirmation")) == 1)
-    confirmation.checked = true;
-  if (JSON.parse(window.localStorage.getItem("AutoJoomerConfirmation")) == 0)
-    confirmation.checked = false;
+
+
+// connect to wifi button
+
+document.addEventListener('DOMContentLoaded', function () {
+
+        document.getElementById("dis_conn").addEventListener("click",connect_wifi)
+       
+    });
+async function connect_wifi(){
+    console.log("clicked")
+    let id = null;
+    clearInterval(id);
+    const elem = document.getElementById("svg_wifi").querySelectorAll("path");   
+    id = setInterval(tower, 5);
+    async function tower() {
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].style.fill= "#4990F5" ;
+            await new Promise(done => setTimeout(() => done(), 200));
+        }
+        
+    }
+    await new Promise(done => setTimeout(() => done(), 1000));
+    window.open("https://msftconnecttest.com/redirect"), "_blank";
+    // let offline=document.getElementById("dis_conn")
+    // let online=document.getElementById("wifi_conn")
+    // offline.style.display="none";
+    // online.style.display="flex";
 }
+
