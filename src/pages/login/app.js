82bb9bcf25e0031a6b232fd39ecc20288c758262
Neeraj P Yetheendran <46917698+NXPY123@users.noreply.github.com>
2022-10-18 11:51:23 +0530

    Replace Timeout on Login Page with Back Button 2/2 (#200)
    * Replaced timeout with button to go back
    
    * Replaced timeout with back button for login page
    
    Co-authored-by: Vishal Lokare <31512838+vishal-lokare@users.noreply.github.com>

--- src/pages/login/app.js
+++ src/pages/login/app.js
@@ -112,6 +112,21 @@ btn.addEventListener("click", () => {
     );
     btn.disabled = true;
     btn.innerText = "Saved";
+
+    //Create button to go back
+    const div = document.getElementsByClassName("form-container")[0];
+    const back_button = document.createElement('BUTTON');
+    back_button.className = "submit-btn";
+    back_button.id = "back";
+    const text = document.createTextNode("Home"); 
+    back_button.onclick = function() { //Redirect to popup
+      window.open("/src/js/popup.html", "_self");
+    }
+    back_button.appendChild(text);
+    //append line break
+    div.appendChild(document.createElement("br"));
+    div.appendChild(back_button);
+
   } else {
     alert("Roll number does not exist");
     return;
