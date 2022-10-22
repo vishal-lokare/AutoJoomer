45334a4fbf9a519706800ded323dd24706a4acc5
Ishaan Mahesh <78961353+ishaan5199@users.noreply.github.com>
2022-10-22 14:40:54 +0530

    Matching up with chrome update branch (#208)
    * dummy password bug
    
    * updates

--- src/pages/login/app.js
+++ src/pages/login/app.js
@@ -4,6 +4,7 @@ const branch = document.querySelector("#branch");
 const batch = document.querySelector("#batch");
 const password = document.querySelector("#password");
 const btn = document.getElementById("mb");
+const backBtn = document.getElementById("backmb");
 let pattern = /20(19BCS((000[1-9])|(00[1-9][0-9])|(01[01][0-9])|(012[0-2]))|(20BCS)((000[1-9])|(00[1-9][0-9])|(01[0-8][0-9])|(0190))|(21BCS)((000[1-9])|(00[1-9][0-9])|(01[0-9][0-9]))|(21BCY)((000[1-9])|(00[1-3][0-9])|(004[0-6]))|(20BEC)((000[1-9])|(00[1-3][0-9]))|(21BEC)((000[1-9])|(00[1-3][0-9])|(004[0-4])))/gim;
 const elements = document.getElementsByTagName('input');
 let firstChange = 0;
@@ -54,8 +55,8 @@ roll.addEventListener("keyup", () => {
       branch.value = "ECE";
     }
   } else if (branchValue == "BCY") {
-    if (year.value != "2019" || year.value != "2020") {
-      branch.value = "BCY";
+    if (year.value != "2019" && year.value != "2020") {
+      branch.value = "CSY";
     }
   }
 });
@@ -64,9 +65,9 @@ roll.addEventListener("keyup", () => {
 var batchValue = "";
 roll.addEventListener("keyup", () => {
   let inputValue = roll.value.toUpperCase();
-  batchValue = inputValue[10];
-  if (year.value != "2019") {
-    if (inputValue.length > 10) {
+  if (inputValue.length > 10) {
+    batchValue = inputValue[10];
+    if (year.value != "2019") {
       if (batchValue % 2 == 0) {
         batch.value = "2";
       } else {
@@ -74,6 +75,10 @@ roll.addEventListener("keyup", () => {
       }
     } else batch.value = "";
   }
+});
+
+backBtn.addEventListener("click", () => {
+  window.open("../../popup.html", "_self");
 });
 
 btn.addEventListener("click", () => {
@@ -100,8 +105,10 @@ btn.addEventListener("click", () => {
     setTimeout(() => { //redirecting to popup.html
       window.open("../../popup.html", "_self");
     }, 1000);
+
   } else {
     alert("Roll number does not exist");
     return;
   }
 });
+
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -8,22 +8,29 @@ window.onload = function populate() {
   });
   document.getElementById("password").value = "******";
   chrome.storage.local.get("AutoJoomerYear", function (result) {
-    year.value = result["AutoJoomerYear"];
+    document.getElementById("year").value = result["AutoJoomerYear"]
   });
   chrome.storage.local.get("AutoJoomerBranch", function (result) {
-    switch(result["AutoJoomerBranch"]) {
-      case "ECE": {
-        branch.value = "ECE";
-      }
-      default: {
-        branch.value = "CSE";
-      }
+    if (result["AutoJoomerBranch"] == "BCS"){
+      document.getElementById("branch").value = "CSE";
+    }
+    else if (result["AutoJoomerBranch"] == "BCY"){
+      document.getElementById("branch").value = "CSY";
+    }
+    if (result["AutoJoomerBranch"] == "BEC"){
+      document.getElementById("branch").value = "ECE";
     }
     // branch.value = result["AutoJoomerBranch"];
   });
   chrome.storage.local.get("AutoJoomerBatch", function (result) {
-    if(result["AutoJoomerBatch"] % 2) batch.value = 1;
-    else batch.value = 2;
-    // document.getElementById("batch").value = result["AutoJoomerBatch"];
+    if (result["AutoJoomerBatch"] == ""){
+      document.getElementById("batch").value = "";
+    }
+    else if (result["AutoJoomerBatch"] % 2 == 1){
+      document.getElementById("batch").value = "1";
+    }
+    else if (result["AutoJoomerBatch"] % 2 == 0){
+      document.getElementById("batch").value = "2";
+    }
   });
 };
