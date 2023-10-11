cc3eeea8d1ae6a00e7ac656ddbb4b8fe6e5c5142
gunjjoshi <gunjjoshi8372@gmail.com>
2023-10-11 11:16:24 +0530

    Removed 2019 regex part

--- src/pages/login/app.js
+++ src/pages/login/app.js
@@ -4,7 +4,7 @@ const branch = document.querySelector("#branch");
 const batch = document.querySelector("#batch");
 const password = document.querySelector("#password");
 const btn = document.getElementById("mb");
-let pattern = /20(19BCS((000[1-9])|(00[1-9][0-9])|(01[01][0-9])|(012[0-2]))|(20BCS)((000[1-9])|(00[1-9][0-9])|(01[0-8][0-9])|(0190))|(21BCS)((000[1-9])|(00[1-9][0-9])|(01[0-9][0-9]))|(21BCY)((000[1-9])|(00[1-3][0-9])|(004[0-6]))|(20BEC)((000[1-9])|(00[1-3][0-9]))|(21BEC)((000[1-9])|(00[1-3][0-9])|(004[0-4])))/gim;
+let pattern = /20(20BCS)((000[1-9])|(00[1-9][0-9])|(01[0-8][0-9])|(0190))|(21BCS)((000[1-9])|(00[1-9][0-9])|(01[0-9][0-9]))|(21BCY)((000[1-9])|(00[1-3][0-9])|(004[0-6]))|(20BEC)((000[1-9])|(00[1-3][0-9]))|(21BEC)((000[1-9])|(00[1-3][0-9])|(004[0-4])))/gim;
 const elements = document.getElementsByTagName('input');
 let firstChange = 0;
 
@@ -20,10 +20,10 @@ function open_autojoomer() {
   window.open("https://www.autojoomer.co/", (target = "_blank"));
 }
 
-for(let i = 0; i < elements.length; i++) {
+for (let i = 0; i < elements.length; i++) {
   elements[i].addEventListener("keyup", () => {
     firstChange++;
-    if (!firstChange) {      
+    if (!firstChange) {
       roll.value = "";
       password.value = "";
       year.value = "";
@@ -42,10 +42,10 @@ roll.addEventListener("keyup", () => {
     year.value = "2019";
     branch.value = "CSE";
     batch.value = "";
-  } 
+  }
   else if (yearValue == 2020) {
     year.value = "2020";
-  } 
+  }
   else if (yearValue == 2021) {
     year.value = "2021";
   }
@@ -58,12 +58,12 @@ roll.addEventListener("keyup", () => {
   branchValue = inputValue.slice(4, 7);
   if (branchValue == "BCS") {
     branch.value = "CSE";
-  } 
+  }
   else if (branchValue == "BEC") {
     if (year.value != "2019") {
       branch.value = "ECE";
     }
-  } 
+  }
   else if (branchValue == "BCY") {
     if (year.value != "2019" && year.value != "2020") {
       branch.value = "CSY";
@@ -81,11 +81,11 @@ roll.addEventListener("keyup", () => {
     if (year.value != "2019") {
       if (batchValue % 2 == 0) {
         batch.value = "2";
-      } 
+      }
       else {
         batch.value = "1";
       }
-    } 
+    }
     else batch.value = "";
   }
 });
@@ -99,8 +99,8 @@ btn.addEventListener("click", () => {
     alert("Invalid Roll Number");
     return;
   }
-  let t =pattern.test(roll.value);
-  if (t) { 
+  let t = pattern.test(roll.value);
+  if (t) {
     alert("You have been Logged In");
     chrome.storage.local.set({ ["AutoJoomerUsername"]: roll.value });
     chrome.storage.local.set({ ["AutoJoomerPassword"]: password.value });
@@ -120,8 +120,8 @@ btn.addEventListener("click", () => {
     const back_button = document.createElement('BUTTON');
     back_button.className = "submit-btn";
     back_button.id = "back";
-    const text = document.createTextNode("Home"); 
-    back_button.onclick = function() { //Redirect to popup
+    const text = document.createTextNode("Home");
+    back_button.onclick = function () { //Redirect to popup
       window.open("/src/js/popup.html", "_self");
     }
     back_button.appendChild(text);
